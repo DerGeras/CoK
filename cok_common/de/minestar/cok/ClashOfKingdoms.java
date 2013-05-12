758012127b25efc708abb5e2027b864490d002c6
Geras <schmittol@hotmail.com>
2013-05-12 19:24:01 +0200

    First commit yay

---
+++ cok_common/de/minestar/cok/ClashOfKingdoms.java
@@ -0,0 +1,44 @@
+package de.minestar.cok;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import de.minestar.cok.references.Reference;
+
+
+@Mod(
+	modid = Reference.MOD_ID,
+	name = Reference.MOD_NAME,
+	version = Reference.MOD_VERSION
+)
+public class ClashOfKingdoms {
+	
+	/**
+	 * Called before the mod is actually loaded
+	 */
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event){
+		
+	}
+	
+	/**
+	 * Called when the mod gets loaded
+	 */
+	@Init
+	public void init(FMLInitializationEvent event){
+		
+	}
+	
+	/**
+	 * Called after all Mods have been loaded.
+	 */
+	@PostInit
+	public void postInit(FMLPostInitializationEvent event){
+		
+	}
+	
+}
---
+++ cok_common/de/minestar/cok/references/Reference.java
@@ -0,0 +1,9 @@
+package de.minestar.cok.references;
+
+public class Reference {
+	
+	public static final String MOD_ID = "ClashOfKingdoms";
+	public static final String MOD_NAME = "Clash of Kingdoms";
+	public static final String MOD_VERSION = "0.0.1";
+	
+}
