458cfc2f8236eefe57f85e247559750b958dac07
Geras <schmittol@hotmail.com>
2013-08-16 16:52:49 +0200

    Fixes #7

--- cok_common/de/minestar/cok/keyhandler/CoKKeyHandler.java
+++ cok_common/de/minestar/cok/keyhandler/CoKKeyHandler.java
@@ -5,6 +5,7 @@ import java.util.EnumSet;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.settings.KeyBinding;
+import cpw.mods.fml.client.FMLClientHandler;
 import cpw.mods.fml.client.registry.KeyBindingRegistry.KeyHandler;
 import cpw.mods.fml.common.TickType;
 import de.minestar.cok.ClashOfKingdoms;
@@ -13,8 +14,6 @@ import de.minestar.cok.references.Reference;
 public class CoKKeyHandler extends KeyHandler{
 	
 	private EnumSet<TickType> tickTypes = EnumSet.of(TickType.CLIENT);
-	
-	public static boolean keyPressed = false;
 
 	public CoKKeyHandler(KeyBinding[] keyBindings, boolean[] repeatings) {
 		super(keyBindings, repeatings);
@@ -29,15 +28,19 @@ public class CoKKeyHandler extends KeyHandler{
 	@Override
 	public void keyDown(EnumSet<TickType> types, KeyBinding kb,
 			boolean tickEnd, boolean isRepeat) {
-		keyPressed = true;
-		EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
-		player.openGui(ClashOfKingdoms.instance, Reference.COK_GUI_ID, player.worldObj,
-				(int) player.posX, (int) player.posY, (int) player.posZ);
+		//Check whether we are in a gui
+		if (FMLClientHandler.instance().getClient().inGameHasFocus) {
+			EntityClientPlayerMP player = Minecraft.getMinecraft().thePlayer;
+			if(player != null){
+				player.openGui(ClashOfKingdoms.instance, Reference.COK_GUI_ID, player.worldObj,
+						(int) player.posX, (int) player.posY, (int) player.posZ);
+			}
+		}
 	}
 
 	@Override
 	public void keyUp(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd) {
-		keyPressed = false;
+		//nothing
 	}
 
 	@Override
