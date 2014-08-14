41d673f9133b10b75b8279a4b8ed4c66fa5bb650
Geras <schmittol@hotmail.com>
2014-08-14 19:20:47 +0200

    Added configurations

--- src/main/java/de/minestar/cok/CoK.java
+++ src/main/java/de/minestar/cok/CoK.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import de.minestar.cok.config.ConfigurationHandler;
 import de.minestar.cok.proxy.IProxy;
 import de.minestar.cok.reference.Reference;
 
@@ -16,19 +17,23 @@ public class CoK {
 	@Mod.Instance(Reference.MOD_ID)
 	public CoK instance;
 	
+	
 	@SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS,
 				serverSide = Reference.SERVER_PROXY_CLASS)
 	public static IProxy proxy;
 	
+	
 	@Mod.EventHandler
 	public void preInit(FMLPreInitializationEvent event){
-		
+		ConfigurationHandler.init(event.getSuggestedConfigurationFile());
 	}
+	
 	
 	@Mod.EventHandler
 	public void init(FMLInitializationEvent event){
 		
 	}
+	
 	
 	@Mod.EventHandler
 	public void postInit(FMLPostInitializationEvent event){
---
+++ src/main/java/de/minestar/cok/util/ItemStackHelper.java
@@ -0,0 +1,15 @@
+package de.minestar.cok.util;
+
+import net.minecraft.item.ItemStack;
+
+public class ItemStackHelper {
+
+	public void setGiven(ItemStack stack){
+		//TODO
+	}
+	
+	public boolean isGiven(ItemStack stack){
+		return false; //TODO
+	}
+	
+}
