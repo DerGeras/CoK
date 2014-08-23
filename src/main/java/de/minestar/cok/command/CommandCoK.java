ad6f9a93171a9836c7974f187a7d3d59611fd9fa
Oliver Schmitt <schmittol@hotmail.com>
2014-08-23 14:29:15 +0200

    Added command to remove games

--- src/main/java/de/minestar/cok/command/CommandCoK.java
+++ src/main/java/de/minestar/cok/command/CommandCoK.java
@@ -3,11 +3,10 @@ package de.minestar.cok.command;
 import java.util.LinkedList;
 import java.util.List;
 
-import cpw.mods.fml.common.registry.GameRegistry;
-
 import net.minecraft.command.ICommandSender;
 import de.minestar.cok.game.CoKGame;
 import de.minestar.cok.game.CoKGameRegistry;
+import de.minestar.cok.game.Team;
 import de.minestar.cok.reference.Reference;
 import de.minestar.cok.util.ChatSendHelper;
 import de.minestar.cok.util.LogHelper;
@@ -21,7 +20,7 @@ public class CommandCoK extends CoKCommand {
 
 	@Override
 	public String getCommandUsage(ICommandSender sender) {
-		return String.format("Usage: %s [create|start|stop] {gameName}", getCommandName());
+		return String.format("Usage: %s [create|remove|start|stop] {gameName}", getCommandName());
 	}
 
 	@Override
@@ -39,6 +38,22 @@ public class CommandCoK extends CoKCommand {
 			CoKGameRegistry.registerGame(args[1]);
 			ChatSendHelper.sendMessageToPlayer(sender, "Game " + args[1] + " created");
 			return;
+		}
+		if(args[0].equals("remove")){
+			if(CoKGameRegistry.registeredGames.containsKey(args[1])){
+				CoKGame game = CoKGameRegistry.registeredGames.get(args[1]);
+				if(game.isRunning()){
+					ChatSendHelper.sendErrorMessageToPlayer(sender, "Cannot remove a running game!");
+					return;
+				}
+				for(Team team : game.getAllTeams()){
+					team.clearPlayers();
+				}
+				CoKGameRegistry.registeredGames.remove(args[1]);
+				ChatSendHelper.sendMessageToPlayer(sender, "The game " + args[1] + " was succesfully removed");
+				return;
+			}
+			ChatSendHelper.sendErrorMessageToPlayer(sender, "Could not find the game " + args[1] + "!");
 		}
 		if(args[0].equals("start")){
 			CoKGame game = CoKGameRegistry.registeredGames.get(args[1]);
@@ -73,9 +88,9 @@ public class CommandCoK extends CoKCommand {
 	public List addTabCompletionOptions(ICommandSender icommandsender, String[] args) {
 		LinkedList<String> list = new LinkedList<String>();
 		if(args.length == 1){
-			addIfPrefixMatches(list, args[0], "create", "start", "stop");
+			addIfPrefixMatches(list, args[0], "create", "remove", "start", "stop");
 		}
-		if(args.length == 2 && (args[0].equals("start") || args[0].equals("stop"))){
+		if(args.length == 2 && (args[0].equals("start") || args[0].equals("stop") || args[0].equals("remove"))){
 			for(String name : CoKGameRegistry.registeredGames.keySet()){
 				addIfPrefixMatches(list, args[1], name);
 			}
